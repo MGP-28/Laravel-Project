2d37b02b8e588489a96f8563f86036a702055ba9
MGP-28 <joaosilvapereira96@hotmail.com>
2022-03-23 23:17:15 +0000

    carousel into container to resize

--- resources/views/components/carousel/carousel.blade.php
+++ resources/views/components/carousel/carousel.blade.php
@@ -1,24 +1,26 @@
-<div id="carouselExampleCaptions" class="carousel slide p-5" data-ride="carousel">
-    <ol class="carousel-indicators">
-        @foreach($images as $image)
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="@if($loop->first) active @endif "></li>
-        @endforeach
-    </ol>
-    <div class="carousel-inner">
-        @foreach($images as $image)
-            @component('components.carousel.carouselblock', [
-                'image' => $image ,
-                'isActive' => ($loop->first)
-                ])
-            @endcomponent
-        @endforeach
+<div class="container">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            @foreach($images as $image)
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="@if($loop->first) active @endif "></li>
+            @endforeach
+        </ol>
+        <div class="carousel-inner">
+            @foreach($images as $image)
+                @component('components.carousel.carouselblock', [
+                    'image' => $image ,
+                    'isActive' => ($loop->first)
+                    ])
+                @endcomponent
+            @endforeach
+        </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </button>
     </div>
-    <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </button>
 </div>
