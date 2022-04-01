582786e7a85185da7ba6f6baaffa27ef9705dac1
MGP-28 <joaosilvapereira96@hotmail.com>
2022-04-01 17:48:10 +0100

    Added address table to database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PersonSeeder::class);
         $this->call(CompanionSeeder::class);
+        $this->call(AddressSeeder::class);
     }
 }
